f1c4223df6a1386e33ef586983803c1899c0e8f7
samandeep007 <115439036+samandeep007@users.noreply.github.com>
2024-07-09 23:48:31 -0600

    feat: Add FindInMountainArray and FindPeakElement classes

--- FindInMountainArray.java
+++ FindInMountainArray.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class FindInMountainArray {
+public class FindInMountainArray {
+    public static void main(String[] args) {
+
+    }
+
+    static int findInMountainArray(int target, int[] mountainArr){
+        int start = 0;
+        int end = arr.length - 1;
+        int
+    }
+
 }
+
--- FindPeakElement.java
+++ FindPeakElement.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class FindPeakElement {
+public class FindPeakElement {
+    public static void main(String[] args){
+        int[] numbers = {4,5,6,7,9,3,2,1};
+        System.out.println(findPeakElement(numbers));
+
+
+    }
+
+    static int findPeakElement(int[] nums){
+        if(nums.length == 1){
+            return 0;
+        }
+        int start = 0;
+        int end = nums.length - 1;
+        while(start<end){
+            int midpoint = start + (end-start) / 2;
+            if(nums[midpoint] < nums[midpoint+1]){
+                start = midpoint + 1;
+            }
+            else{
+                end = midpoint;
+            }
+        }
+        return end;
+    }
 }
