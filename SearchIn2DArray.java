4cd5aa76f53e18c6d4c920fba2b9c41d0759ccb8
samandeep007 <115439036+samandeep007@users.noreply.github.com>
2024-07-11 10:51:47 -0600

    Refactor binary search implementation and add searchIn2DArray method

--- SearchIn2DArray.java
+++ SearchIn2DArray.java
@@ -1,30 +1,61 @@
+import java.util.Arrays;
+
 public class SearchIn2DArray {
     public static void main(String[] args){
         int[][] numbers = {{1,2,3}, {7,9,10}, {11,12,18}};
-        int target = 19;
-        System.out.println(searchIn2DArray(numbers, target));
+        int target = 12;
+        System.out.println(Arrays.toString(searchIn2DArray(numbers, target)));
     }
 
-    static boolean searchIn2DArray(int[][] arr, int target) {
+    static int[] searchIn2DArray(int[][] matrix, int target) {
         int start = 0;
-        int end = arr.length - 1;
+        int end = matrix.length - 1;
         while (start <= end) {
+
             int midpoint = start + (end - start) / 2;
-            if (arr[midpoint][0] == target) {
-                return true;
+            if (matrix[midpoint][0] == target) {
+                return new int[]{midpoint, 0};
             }
-            if (arr[midpoint][0] > target) {
-                end = midpoint - 1;
-            } else {
-                int index = binarySearch(arr[midpoint], target);
-                if (index == -1) {
-                    start = midpoint + 1;
-                } else {
-                    return true;
+
+            if(matrix[midpoint][matrix[midpoint].length - 1] == target){
+                return new int[]{midpoint, matrix[midpoint].length-1};
+            }
+
+            if(matrix[midpoint][0] < target && matrix[midpoint][matrix[midpoint].length - 1] > target){
+
+                int index = binarySearch(matrix[midpoint], target);
+                if(index!=-1){
+                    return new int[]{midpoint, index};
                 }
+                return new int[]{-1, -1};
+            }
+
+            else if (matrix[midpoint][0] > target) {
+                end = midpoint - 1;
+            }
+
+            else {
+                   start = midpoint + 1;
             }
         }
-        return false;
+        return new int[]{-1, -1};
+    }
+
+    static int[] searchInMatrix(int[][] matrix, int target){
+        int row = 0;
+        int column = matrix.length - 1;
+        while(row < matrix.length && column >= 0){
+            if(matrix[row][column] == target ){
+                return new int[]{row, column};
+            }
+            if(matrix[row][column] > target){
+                column--;
+            }
+            else {
+                row++;
+            }
+        }
+        return new int[]{-1,-1};
     }
 
 
