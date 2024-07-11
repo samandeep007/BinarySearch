0801403d3020a3644d9fdad8e064ea4cd8323820
samandeep007 <115439036+samandeep007@users.noreply.github.com>
2024-07-11 09:23:35 -0600

    Refactor binary search implementation and add searchIn2DArray method

--- SearchIn2DArray.java
+++ SearchIn2DArray.java
@@ -1,2 +1,49 @@
-package PACKAGE_NAME;public class SearchIn2DArray {
+public class SearchIn2DArray {
+    public static void main(String[] args){
+        int[][] numbers = {{1,2,3}, {7,9,10}, {11,12,18}};
+        int target = 19;
+        System.out.println(searchIn2DArray(numbers, target));
+    }
+
+    static boolean searchIn2DArray(int[][] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end) {
+            int midpoint = start + (end - start) / 2;
+            if (arr[midpoint][0] == target) {
+                return true;
+            }
+            if (arr[midpoint][0] > target) {
+                end = midpoint - 1;
+            } else {
+                int index = binarySearch(arr[midpoint], target);
+                if (index == -1) {
+                    start = midpoint + 1;
+                } else {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+
+    static int binarySearch(int[] arr, int target){
+        int start = 0;
+        int end = arr.length - 1;
+        while(start<=end){
+            int midpoint = start + (end-start)/2;
+            if(arr[midpoint] == target){
+                return midpoint;
+            }
+            else if(arr[midpoint]>target){
+                end = midpoint - 1;
+            }
+
+            else {
+                start = midpoint + 1;
+            }
+        }
+        return -1;
+    }
 }
